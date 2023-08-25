2e91319cd45866d88114599cc3f8cb5d2cb48f5f
mk <c1002405@planet.kanazawa-it.ac.jp>
2023-08-25 22:59:03 +0900

    8章完了

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
-import { headers, cookies } from "next/headers";
 
 export async function GET() {
-    const headersList = headers();
-    const cookieStore = cookies();
-
-    console.log('headersList', headersList);
-    console.log('cookieStore', cookieStore);
-
+    console.log('GET Request');
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const date = await response.json();
-    return NextResponse.json(date);
+    const data = await response.json();
+    return NextResponse.json(data);
+}
+
+export async function POST(request: Request){
+    const res = await request.json();
+    return NextResponse.json({ res });
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,23 @@
 import UserList from "./UserList";
-type User = {
-    id:string,
-    name:string,
-    email:string,
-};
 
 const Page = async () => {
+    const response = await fetch('http://localhost:3000/api',{
+        //cache: 'no-store'
+        next: {revalidate: 5 },
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: 'john',
+            email: 'john@example.com',
+        }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
     return (
         <div className="m-4">
             <h1 className="text-lg font-blod">ユーザ一覧</h1>
