135bd985cde2ffc20be192da8b22fea0b5a8d9a5
mk <c1002405@planet.kanazawa-it.ac.jp>
2023-09-24 21:00:43 +0900

    9.1.2

--- app/users/UserList.tsx
+++ app/users/UserList.tsx
@@ -1,22 +1,22 @@
-type User = {
+import Link from 'next/link';
+
+export type User = {
     id: string,
     name: string,
     email: string,
 };
 
 const UserList = async () => {
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch('http://localhost:3000/api?name=john',{
-        cache: 'no-store',
-});
-    if (!response.ok) throw new Error('Failed to fetch data');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users: User[] = await response.json();
     return (
-        <ul>
-            {users.map((user) => (
-                <li key={user.id}>{user.name}</li>
-            ))}
-        </ul>
+    <ul>
+        {users.map((user) => (
+        <li key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+        ))}
+    </ul>
     );
 };
 
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,28 +1,11 @@
-import UserList from "./UserList";
+import UserList from './UserList';
 
 const Page = async () => {
-    const response = await fetch('http://localhost:3000/api',{
-        //cache: 'no-store'
-        next: {revalidate: 5 },
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: 'john',
-            email: 'john@example.com',
-        }),
-    });
-
-    const data = await response.json();
-
-    console.log(data);
-
     return (
-        <div className="m-4">
-            <h1 className="text-lg font-blod">ユーザ一覧</h1>
-            <UserList />
-        </div>
+    <div className="m-4">
+        <h1 className="text-lg font-bold">ユーザ一覧</h1>
+        <UserList />
+    </div>
     );
 };
 
